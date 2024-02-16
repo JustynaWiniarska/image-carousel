a55dec8d951b2a8be327561ddd29881bc035ac60
Justyna <justyna.field@gmail.com>
2024-02-16 16:56:21 -0600

    Extracts image urls

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
+
+const imageUrls = ref([])
 
 
-const fetchImages = async () => {
+const fetchData = async () => {
   const response = await fetch(`https://www.reddit.com/r/aww/top/.json?t=all`)
   const jsonResponse = await response.json()
   const data = jsonResponse.data.children
-  console.log(data)
+
+  data.map((item) => {
+    imageUrls.value.push(item.data.url)
+  })
+
+  console.log(imageUrls.value)
 }
 
+
+
+
 onMounted(() => {
-  fetchImages()
+  fetchData()
 })
 
 </script>
@@ -19,7 +29,11 @@ onMounted(() => {
 <template>
   <div>
     Image Carousel
-    <div class="border-2 border-teal-600 w-full h-full"></div>
+    <div 
+      class="border-2 border-teal-600 w-full h-[550px]"
+      style="background-image: url('https://th.bing.com/th/id/R.76312dfb95d1c1bdb1dec80fd71fa45f?rik=u9WkKnH%2bZTi%2fpg&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f07%2fHD-Peaceful-Backgrounds.jpg&ehk=VI7X2ZKR%2bgNtV0t5oSJfEcz%2fLs7ugAhZZDw5il%2fOg3I%3d&risl=&pid=ImgRaw&r=0')"
+    >
+    </div>
   </div>
 </template>
 
