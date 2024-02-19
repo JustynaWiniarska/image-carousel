85bf0e0bac08cdfd9cd5f2a768752711e7d7565b
Justyna <justyna.field@gmail.com>
2024-02-18 20:23:28 -0600

    Loads images on the page

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,36 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref , onMounted} from 'vue';
 
-const imageUrls = ref([])
-
+const imageData = ref([])
 
 const fetchData = async () => {
-  const response = await fetch(`https://www.reddit.com/r/aww/top/.json?t=all`)
-  const jsonResponse = await response.json()
-  const data = jsonResponse.data.children
-
-  data.map((item) => {
-    imageUrls.value.push(item.data.url)
-  })
-
-  console.log(imageUrls.value)
+  try {
+    const response = await fetch('https://www.reddit.com/r/aww/top/.json?t=all')
+    const jsonResponse = await response.json()
+    const data = jsonResponse.data.children.map(item => item.data)
+   
+    imageData.value = data
+  } catch (error) {
+    console.error('Error catching data: ', error)
+  }
 }
-
-
-
 
 onMounted(() => {
   fetchData()
 })
-
 </script>
 
 
 <template>
   <div>
     Image Carousel
-    <div 
-      class="border-2 border-teal-600 w-full h-[550px]"
-      style="background-image: url('https://th.bing.com/th/id/R.76312dfb95d1c1bdb1dec80fd71fa45f?rik=u9WkKnH%2bZTi%2fpg&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f07%2fHD-Peaceful-Backgrounds.jpg&ehk=VI7X2ZKR%2bgNtV0t5oSJfEcz%2fLs7ugAhZZDw5il%2fOg3I%3d&risl=&pid=ImgRaw&r=0')"
-    >
+    <div>
+      <div 
+        v-for="(item, index) in imageData"
+        :key="index"
+      >
+        <img :src="item.url" />
+      </div>
     </div>
   </div>
 </template>
